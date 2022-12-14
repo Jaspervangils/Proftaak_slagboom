603f1a5eceed47baf60d124501db70939cdb939d
unknown <i476949@fhict.nl>
2022-12-14 11:48:35 +0100

    code aangepast op proftaak database

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"io/ioutil"
 	"log"
 
@@ -42,34 +41,35 @@ func databaseFunc(p string) string {
 	var resp string  //p is een parameter die is meegegeven in de url van de API req
 	if len(p) != 8 { //filter alles wat niet dezelfde lengte heeft als een kenteken er uit
 		resp = "geen kenteken"
-	}
-
-	db, err := sql.Open("mysql", configStruct.Database.Username+":"+configStruct.Database.Password+"@tcp("+configStruct.Host+")/csdb?tls=true")
-	if err != nil { //open SQL verbinding met ww, naam en server uit conf bestand
-		fmt.Println(err)
-	}
-	defer db.Close()
-	rows, err := db.Query("SELECT * FROM `csdb`.`kenteken` WHERE KentekenID = \"" + p + "\"")
-	if err != nil { //de query die de database ontvangt met p als ingevoerd kenteken
-		resp = "code 0"
 	} else {
-		for rows.Next() { // zet resultaat van de query in een struct
-			var result Result
-			err := rows.Scan(&result.KentekenResult)
-			if err != nil {
-				resp = "code 1"
+		db, err := sql.Open("mysql", configStruct.Database.Username+":"+configStruct.Database.Password+"@tcp("+configStruct.Host+")/fonteyn-internal-db?tls=true")
+		if err != nil { //open SQL verbinding met ww, naam en server uit conf bestand
+			resp = "connectie err"
+		} else {
+			defer db.Close()
+			rows, err := db.Query("SELECT kenteken FROM `fonteyn-internal-db`.`kenteken` WHERE kenteken = \"" + p + "\"")
+			if err != nil { //de query die de database ontvangt met p als ingevoerd kenteken
+				resp = "fout database"
+			} else {
+				for rows.Next() { // zet resultaat van de query in een struct
+					var result Result
+					err := rows.Scan(&result.KentekenResult)
+					if err != nil {
+						resp = "false"
+					} else {
+						resp = result.KentekenResult
+					}
+				}
 			}
-			resp = result.KentekenResult
 		}
 	}
 	return resp // returned een string met het kenteken of foutcode
-
 }
 
 func input() {
 	app := fiber.New() //api server
 
-	app.Get("api/:id", func(c *fiber.Ctx) error {
+	app.Get("/:id", func(c *fiber.Ctx) error {
 		param := c.Params("id")
 		out := databaseFunc(param)
 		return c.SendString(out)
@@ -77,29 +77,50 @@ func input() {
 	log.Fatal(app.Listen(":3000"))
 }
 
-// var (
-// 	ceifers string = "0123456789"
-// 	letters string = "DFGHJKLNPRSTXYZ"
-// )
+// func test2() {
+// 	app := fiber.New() //api server
 
-// func kentekenGen() string {
-// 	rand.Seed(time.Now().Unix())
+// 	app.Get("/api", func(c *fiber.Ctx) error {
+// 		out := test()
+// 		return c.SendString(out)
+// 	})
+// 	log.Fatal(app.Listen(":3000"))
+// }
 
-// 	var kenteken strings.Builder
+// func test() string {
+// 	file, err := ioutil.ReadFile("config.yaml") // laad configuratie bestand
 
-// 	random := rand.Intn(len(letters))
-// 	kenteken.WriteString(string(letters[random]))
-// 	kenteken.WriteString("-")
-
-// 	for i := 0; i < 3; i++ {
-// 		randomC := rand.Intn(len(ceifers))
-// 		kenteken.WriteString(string(ceifers[randomC]))
+// 	if err != nil {
+// 		panic(err)
 // 	}
-// 	kenteken.WriteString("-")
-// 	for i := 0; i < 2; i++ {
-// 		randomC := rand.Intn(len(letters))
-// 		kenteken.WriteString(string(letters[randomC]))
+// 	var configStruct Config
+
+// 	err = yaml.Unmarshal(file, &configStruct)
+// 	if err != nil {
+// 		panic(err)
 // 	}
 
-// 	return kenteken.String()
+// 	var resp string
+
+// 	db, err := sql.Open("mysql", configStruct.Database.Username+":"+configStruct.Database.Password+"@tcp("+configStruct.Host+")/csdb?tls=true")
+// 	if err != nil { //open SQL verbinding met ww, naam en server uit conf bestand
+// 		resp = "connectie err"
+// 	}
+// 	defer db.Close()
+// 	rows, err := db.Query("SELECT kenteken FROM `fonteyn-internal-db`.`kenteken` WHERE kenteken = \"12345678\"")
+// 	if err != nil { //de query die de database ontvangt met p als ingevoerd kenteken
+// 		resp = "fout database"
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(rows)
+// 	for rows.Next() { // zet resultaat van de query in een struct
+// 		var result Result
+// 		err := rows.Scan(&result.KentekenResult)
+// 		if err != nil {
+// 			resp = "false"
+// 		} else {
+// 			resp = result.KentekenResult
+// 		}
+// 	}
+// 	return resp
 // }
